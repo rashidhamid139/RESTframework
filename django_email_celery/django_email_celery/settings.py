dd6494bfe4f998e0223c18a067de7680d9f0f009
rashidhamid139 <rashidhamid139@gmail.com>
2020-08-06 11:44:45 +0530

    Sending mail using celery

--- django_email_celery/django_email_celery/settings.py
+++ django_email_celery/django_email_celery/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'rashidhamid139@gmail.com'
-EMAIL_HOST_PASSWORD = 'jkcgtsdpypggeetz'
+EMAIL_HOST_PASSWORD = 'jkcgtsdpddddypggeetz'
 EMAIL_PORT = 587
